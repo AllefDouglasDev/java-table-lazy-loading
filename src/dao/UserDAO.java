879b5de6f487b88439f9148b54809202f7a7cce3
Allef Douglas <allef.douglas@aftisolucoes.com.br>
2019-06-14 16:30:30 -0300

    Add comment in UserDAO to get the data

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -7,15 +7,36 @@ import java.util.ArrayList;
 
 public class UserDAO {
 
-    public int getUsersSize() {
+    /**
+     * Get all data by the query passed by param
+     * Execute the query and return the length of the result
+     */
+    public int getUsersSize (String query) {
         return 200;
     }
 
-    public Paginate<User> getUsers(int perPage, int page) {
+    public Paginate<User> getUsers (int perPage, int page) {
         ArrayList<User> users = new ArrayList<>();
-        int usersSize = getUsersSize();
+        /**
+         * Query then will be executed to get the users
+         */
+        String query = "SELECT * FROM users";
+        int usersSize = getUsersSize(query);
         int sizeToLoad = Paginate.calculateSizeToLoad(perPage, page, usersSize);
         if (sizeToLoad > 0) {
+            /**
+             * Use the query in limited way to get the data
+             */
+            query = query + " LIMIT ?, ?";
+            /**
+             * Execute the query to get all users passing the offset and perPage
+             * ------ Calculate the offset ------
+             * int offset = (page - 1) * perPage;
+             *
+             * use:
+             * stmt.setInt(1, offset);
+             * stmt.setInt(2, perPage);
+             */
             for (int i = 0; i < sizeToLoad; i++) {
                 users.add(new User(
                         generateId(),
